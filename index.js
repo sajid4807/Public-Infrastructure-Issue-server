54f4fc6c62173a66334f9dbd168a09f1644735b2
sajid4807 <mdsajidhossen4807@gmail.com>
2025-12-13 21:21:33 +0600

    implement delete api

--- index.js
+++ index.js
@@ -60,6 +60,13 @@ async function run() {
       const result = await reportCollection.insertOne(report);
       res.send(result);
     });
+    app.delete("/reports/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+     
+      const result = await reportCollection.deleteOne(query);
+      res.send(result);
+    });
 
     await client.db("admin").command({ ping: 1 });
     console.log(
